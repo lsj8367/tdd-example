35e1e9b4ee5f4465aac8d25937d959c3ae720f83
lsj8367 <lsj8367@naver.com>
2022-04-14 00:16:20 +0900

    :sparkles: 동일성 비교로 테스트를 교체

--- src/test/java/io/github/lsj8367/domain/MoneyTest.java
+++ src/test/java/io/github/lsj8367/domain/MoneyTest.java
@@ -1,6 +1,8 @@
 package io.github.lsj8367.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,17 +20,26 @@ class MoneyTest {
     @DisplayName("Dollar 객체에 times 횟수만큼 곱하면 곱셈이 된다.")
     void testMultiplication() {
         five = five.times(2);
-        assertThat(five.amount).isEqualTo(10);
+        assertThat(five).isEqualTo(new Dollar(10));
     }
 
     @Test
     @DisplayName("곱해주면 새로운 값을 가지는 불변객체를 만든다.")
     void multipleContinue() {
         Dollar product = five.times(2);
-        assertThat(product.amount).isEqualTo(10);
+        assertThat(product).isEqualTo(new Dollar(10));
 
         product = five.times(3);
-        assertThat(product.amount).isEqualTo(15);
+        assertThat(product).isEqualTo(new Dollar(15));
+    }
+
+    @Test
+    @DisplayName("객체끼리 값을 비교한다.")
+    void equalTest() {
+        assertAll(
+            () -> assertThat(five).isEqualTo(new Dollar(5)),
+            () -> assertThat(five).isNotEqualTo(new Dollar(6))
+        );
     }
 
 }
